bd62b38ea9e4c3448a68eb7d8337fa24fb966375
berkay-yuksel <98.berkay.yuksel@gmail.com>
2021-09-29 18:15:26 +0300

    footer and infographic sections svg updates

--- components/Infographic.js
+++ components/Infographic.js
@@ -7,25 +7,25 @@ import Image from 'next/image'
 const Infographic =()=>{
     return <div className={styles.igfwrapper}>
         <div className={styles.igfcard} >
-        <Image  src="/international.svg" alt="Vercel Logo" width={100} height={100} />
+        <Image  src="/earth-grid.png" alt="Vercel Logo" width={100} height={100} />
 <h1>14</h1>
 <p>Ulusal ve Uluslararası Şirket</p>
         </div>
 
         <div className={styles.igfcard} >
-        <Image  src="/investor.svg" alt="Vercel Logo" width={100} height={100} />
+        <Image  src="/save-money.png" alt="Vercel Logo" width={100} height={100} />
 <h1>5</h1>
 <p>Uluslararası Yatırımcı</p>
         </div>
 
         <div className={styles.igfcard} >
-        <Image  src="/offices.svg" alt="Vercel Logo" width={100} height={100} />
+        <Image  src="/building.png" alt="Vercel Logo" width={100} height={100} />
 <h1>4</h1>
 <p>Merkez Ofis</p>
         </div>
 
         <div className={styles.igfcard} >
-        <Image  src="/workers.svg" alt="Vercel Logo" width={100} height={100} />
+        <Image  src="/service.png" alt="Vercel Logo" width={100} height={100} />
 <h1>100+</h1>
 <p>Personel</p>
         </div>
